b0f26cb2eab305f02d3038d96c8d502707d2cefd
jmarthala-design <jmarthala@outlook.com>
2025-08-25 20:37:12 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Basic form submit behavior
-const subscribeForm = document.querySelector('.subscribe form');
-subscribeForm?.addEventListener('submit', function(e){
-  e.preventDefault();
-  alert('Thank you for subscribing!');
-});
